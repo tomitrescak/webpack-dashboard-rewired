f10adaf6459fcb5d83b165016523eb1de42b2e6a
Tomas Trescak <tomi.trescak@gmail.com>
2018-05-02 07:57:15 +1000

    Initial Commit With Typescript

---
+++ config-overrides.js
@@ -0,0 +1,29 @@
+const { compose } = require('react-app-rewired');
+const {
+  rewireWebpack: rewireTypescript,
+  rewireJest: rewireTypescriptJest
+} = require("react-app-rewire-typescript-babel-preset");
+const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+
+function rewireLoaderPlugin(config, env, definePluginOptions = {}) {
+  // Add the define plugin to the list of plugins
+  config.plugins = (config.plugins || [])
+    .concat([new DashboardPlugin()])
+  return config
+}
+
+module.exports = {
+  webpack: function(config, env) {
+    const rewires = compose(
+      rewireTypescript,
+      rewireReactHotLoader,
+      rewireLoaderPlugin
+    );
+
+    return rewires(config, env);
+  },
+  jest: function(config) {
+    return rewireTypescriptJest(config);
+  }
+};
